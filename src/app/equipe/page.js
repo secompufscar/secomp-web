bbc0ab763b2aedf04be8e3d3e388cffa10c80db9
KY-U <uscaio@estudante.ufscar.br>
2023-09-12 20:20:22 -0300

    add: navBar

---
+++ src/app/equipe/page.js
@@ -0,0 +1,18 @@
+//navBar
+import NavBar from '../../components/NavBar'
+import '../desconto/globals.css'
+
+import styles from './page.module.css'
+
+
+
+export default function Equipe() {
+    return (
+        <>
+            <NavBar />
+            <main className={styles.main}>
+
+            </main>
+        </>
+    )
+}
