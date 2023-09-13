6011800fd6ebd17e6eea8c7b831ed309f704bb41
KY-U <uscaio@estudante.ufscar.br>
2023-09-12 23:11:56 -0300

    add: perfil de membro de equipe e informações na base da página

---
+++ src/app/equipe/EquipeMolde.js
@@ -0,0 +1,22 @@
+import React from 'react';
+import Image from 'next/image';
+import styles from './style.module.css'
+
+
+const ImageWithText = ({ imageUrl, altText,title, text,obs }) => {
+  return (
+    <div className={styles.equipeMolde}>
+      <div className={styles.imageContainer}>
+        <Image className={styles.imageMembro} src={imageUrl} alt={altText} width={40} height={40} />
+      </div>
+      <div className={styles.containerText}>
+        <h2>{title}</h2>
+        <p className={styles.text}>{text}</p>
+        <p className={styles.obs}>{obs}</p>
+      </div>
+      
+    </div>
+  );
+};
+
+export default ImageWithText;
--- src/app/equipe/page.js
+++ src/app/equipe/page.js
@@ -2,7 +2,10 @@
 import NavBar from '../../components/NavBar'
 import '../desconto/globals.css'
 
-import styles from './page.module.css'
+//imageWithText
+import EquipeMolde from './EquipeMolde';
+
+import styles from './style.module.css'
 
 
 
@@ -11,8 +14,19 @@ export default function Equipe() {
         <>
             <NavBar />
             <main className={styles.main}>
-
+                <EquipeMolde imageUrl="/bochi-meme.gif" altText="Image Alt Text"
+                title='Ulisflávio - Presidente do Brasil'
+                text="Equipe T.I" 
+                obs='contato: uscaio@estudante.ufscar.br' />
+                
             </main>
+            <div className={styles.bottomText}>  
+                    <p>Desenvolvido por Hitori Gotoh, Ryo Yamada, Ikuyo Kitae Nijika Ijichi</p>
+                    <p>Departamento de Computação © 2023 - Todos os direitos reservados</p>
+                    <p>Universidade Federal de São Carlos - Campus São Carlos</p>
+                    <p>Rodovia Washington Luís, km 235 - SP-310</p>
+                    <p>São Carlos - São Paulo - Brasil CEP 13565-905</p>
+            </div>
         </>
     )
 }
