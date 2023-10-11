f1eb3f6a26ebe75484a64eb90f754984e9bf16bc
@jakson.huang <jakson.huang@catijr.com.br>
2023-10-11 17:06:54 -0300

    feat: fazendo a tela de patrocinadores

--- tailwind.config.js
+++ tailwind.config.js
@@ -14,6 +14,7 @@ module.exports = {
   theme: {
     fontFamily: {
       satoshi: ["Satoshi", "sans-serif"],
+      montserrat: ["Montserrat", "sans-serif"],
     },
     colors: {
       current: "currentColor",
