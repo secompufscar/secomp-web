6a8ec5e3925d47c933a3a3a0dda5477954797ad6
0Notzura <111460844+0Notzura@users.noreply.github.com>
2023-10-12 13:35:02 -0300

    Add files via upload

--- src/app/desconto/ImagewithText.js
+++ src/app/desconto/ImagewithText.js
@@ -1,20 +1,17 @@
 import React from 'react';
 import Image from 'next/image';
-import styles from './style.module.css'
 
-
-const ImageWithText = ({ imageUrl, altText,title, text,obs }) => {
+const ImageWithText = ({ imageUrl, altText, title, text, obs }) => {
   return (
-    <div className={styles.imageWithText}>
-      <div className={styles.imageContainer}>
-        <Image className={styles.imageDescount} src={imageUrl} alt={altText} width={40} height={40} />
+    <div className="flex flex-col items-center gap-4 p-4 rounded-lg w-80 md:w-96 min-w-[20rem] bg-gradient-to-r from-[#008CB5] via-[#00CEA1] to-[#00CEA1] shadow-md transition-transform hover:-translate-y-0.5 hover:scale-105 hover:shadow-lg mt-4 mb-4">
+      <div className="flex-shrink-0 w-24 md:w-28">
+        <Image className="w-full h-auto" src={imageUrl} alt={altText} width={40} height={40} />
       </div>
-      <div className={styles.containerText}>
-        <h2>{title}</h2>
-        <p className={styles.text}>{text}</p>
-        <p className={styles.obs}>{obs}</p>
+      <div className="w-full text-center text-shadow-[2px 2px 4px rgba(0, 0, 0, 0.5)]">
+        <h2 className="text-2xl font-bold">{title}</h2>
+        <p className="text-lg font-bold text-white">{text}</p>
+        <p className="text-base">{obs}</p>
       </div>
-      
     </div>
   );
 };
