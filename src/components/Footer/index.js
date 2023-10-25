b350f7d474982887b533207d34cb3cdafde8041e
Vitorrecoaro <vitordearec@gmail.com>
2023-10-25 00:13:08 -0300

    feat: Responsive home page components

--- src/components/Footer/index.js
+++ src/components/Footer/index.js
@@ -1,51 +1,64 @@
-import React from 'react';
-import Image from 'next/image'
+import React from "react";
+import Image from "next/image";
 
-import './styles.css'
+import "./styles.css";
 
 const Footer = () => {
     return (
-        <footer className='backgroundStyle flex flex-row items-center'>
+        <footer className="backgroundStyle flex flex-row items-center">
             <div className="w-60 container mx-auto px-4">
-                <Image className="w-full h-auto" src="/secompUfscar.png" width={40} height={40}/>
+                <Image className="w-full h-auto" src="/secompUfscar.png" width={40} height={40} alt="logo secomp" />
             </div>
             <div className="w-40 container mx-auto px-4">
-                <p className="text-white text-sm">
-                    Realização:
-                </p>
-                <Image className="w-full h-auto" src="/ufscar_vector.png" width={40} height={40}/>
+                <p className="text-white text-sm">Realização:</p>
+                <Image className="w-full h-auto" src="/ufscar_vector.png" width={40} height={40} alt="logo ufscar" />
             </div>
             <div className="w-5 container mx-auto px-4">
                 <p className="w-50 text-white text-sm">
-                    <a href='https://site.dc.ufscar.br' class="underline"> Departamento de Computação (UFSCAR)</a>
-                </p>   
+                    <a href="https://site.dc.ufscar.br" className="underline">
+                        {" "}
+                        Departamento de Computação (UFSCAR)
+                    </a>
+                </p>
                 <p className="w-65 text-white text-sm">
                     Rodovia Washington Luís-Km 235-SP-310-São Carlos-SP-CEP-13565-905
-                </p> 
+                </p>
                 <p className="w-40 text-white text-sm mt-15">
-                    <a href='mailto: suporte@secompufscar.com.br' class="underline">Dúvidas ou sugestões?</a>
-                </p>    
+                    <a href="mailto: suporte@secompufscar.com.br" className="underline">
+                        Dúvidas ou sugestões?
+                    </a>
+                </p>
             </div>
 
             <div className="w-5 container mx-auto px-4">
-            < button href="https://www.instagram.com/secompufscar/" class="inline-flex items-center justify-center">
-                <svg
-                xmlns="https://www.instagram.com/secompufscar/"
-                class="h-5 w-5"
-                fill="currentColor"
-                viewBox="0 0 24 24">
-                <path
-                  d="M12 2.163c3.204 0 3.584.012 4.85.07 3.252.148 4.771 1.691 4.919 4.919.058 1.265.069 1.645.069 4.849 0 3.205-.012 3.584-.069 4.849-.149 3.225-1.664 4.771-4.919 4.919-1.266.058-1.644.07-4.85.07-3.204 0-3.584-.012-4.849-.07-3.26-.149-4.771-1.699-4.919-4.92-.058-1.265-.07-1.644-.07-4.849 0-3.204.013-3.583.07-4.849.149-3.227 1.664-4.771 4.919-4.919 1.266-.057 1.645-.069 4.849-.069zm0-2.163c-3.259 0-3.667.014-4.947.072-4.358.2-6.78 2.618-6.98 6.98-.059 1.281-.073 1.689-.073 4.948 0 3.259.014 3.668.072 4.948.2 4.358 2.618 6.78 6.98 6.98 1.281.058 1.689.072 4.948.072 3.259 0 3.668-.014 4.948-.072 4.354-.2 6.782-2.618 6.979-6.98.059-1.28.073-1.689.073-4.948 0-3.259-.014-3.667-.072-4.947-.196-4.354-2.617-6.78-6.979-6.98-1.281-.059-1.69-.073-4.949-.073zm0 5.838c-3.403 0-6.162 2.759-6.162 6.162s2.759 6.163 6.162 6.163 6.162-2.759 6.162-6.163c0-3.403-2.759-6.162-6.162-6.162zm0 10.162c-2.209 0-4-1.79-4-4 0-2.209 1.791-4 4-4s4 1.791 4 4c0 2.21-1.791 4-4 4zm6.406-11.845c-.796 0-1.441.645-1.441 1.44s.645 1.44 1.441 1.44c.795 0 1.439-.645 1.439-1.44s-.644-1.44-1.439-1.44z" />
-              </svg>
-            </button>
+                <button
+                    href="https://www.instagram.com/secompufscar/"
+                    className="inline-flex items-center justify-center">
+                    <svg
+                        xmlns="https://www.instagram.com/secompufscar/"
+                        className="h-5 w-5"
+                        fill="currentColor"
+                        viewBox="0 0 24 24">
+                        <path d="M12 2.163c3.204 0 3.584.012 4.85.07 3.252.148 4.771 1.691 4.919 4.919.058 1.265.069 1.645.069 4.849 0 3.205-.012 3.584-.069 4.849-.149 3.225-1.664 4.771-4.919 4.919-1.266.058-1.644.07-4.85.07-3.204 0-3.584-.012-4.849-.07-3.26-.149-4.771-1.699-4.919-4.92-.058-1.265-.07-1.644-.07-4.849 0-3.204.013-3.583.07-4.849.149-3.227 1.664-4.771 4.919-4.919 1.266-.057 1.645-.069 4.849-.069zm0-2.163c-3.259 0-3.667.014-4.947.072-4.358.2-6.78 2.618-6.98 6.98-.059 1.281-.073 1.689-.073 4.948 0 3.259.014 3.668.072 4.948.2 4.358 2.618 6.78 6.98 6.98 1.281.058 1.689.072 4.948.072 3.259 0 3.668-.014 4.948-.072 4.354-.2 6.782-2.618 6.979-6.98.059-1.28.073-1.689.073-4.948 0-3.259-.014-3.667-.072-4.947-.196-4.354-2.617-6.78-6.979-6.98-1.281-.059-1.69-.073-4.949-.073zm0 5.838c-3.403 0-6.162 2.759-6.162 6.162s2.759 6.163 6.162 6.163 6.162-2.759 6.162-6.163c0-3.403-2.759-6.162-6.162-6.162zm0 10.162c-2.209 0-4-1.79-4-4 0-2.209 1.791-4 4-4s4 1.791 4 4c0 2.21-1.791 4-4 4zm6.406-11.845c-.796 0-1.441.645-1.441 1.44s.645 1.44 1.441 1.44c.795 0 1.439-.645 1.439-1.44s-.644-1.44-1.439-1.44z" />
+                    </svg>
+                </button>
                 <p className="w-50 text-white text-sm">
-                    <a href='https://www.instagram.com/secompufscar/' class="underline"> não consigo fazer o link funcionar da imagem</a>
-                </p>   
+                    <a href="https://www.instagram.com/secompufscar/" className="underline">
+                        {" "}
+                        não consigo fazer o link funcionar da imagem
+                    </a>
+                </p>
                 <p className="w-50 text-white text-sm">
-                    <a href='https://www.facebook.com/secompufscar' class="underline"> Facebook</a>
-                </p> 
+                    <a href="https://www.facebook.com/secompufscar" className="underline">
+                        {" "}
+                        Facebook
+                    </a>
+                </p>
                 <p className="w-50 text-white text-sm">
-                    <a href='https://www.linkedin.com/company/secompufscar/' class="underline"> Linkedin</a>
+                    <a href="https://www.linkedin.com/company/secompufscar/" className="underline">
+                        {" "}
+                        Linkedin
+                    </a>
                 </p>
             </div>
         </footer>
