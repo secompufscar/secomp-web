a4364ed9aad16593785fe2febb2cbc3a66de7f10
Enzo Murayama <enzomurayama@gmail.com>
2025-08-25 03:38:26 -0300

    refactor: update countdown

--- src/components/Countdown/index.js
+++ src/components/Countdown/index.js
@@ -1,28 +1,42 @@
 'use client'
+import { useEffect, useState } from "react";
 import UseCountdown from "@/app/hooks/useCountdown";
 import { oswald, robotoMono } from "@/utils/fonts"
 
 export default function Countdown() {
-    const countdown = UseCountdown(new Date('2025/09/29').getTime());
+  const countdown = UseCountdown(new Date('2025/09/29').getTime());
+  const [show, setShow] = useState(false);
 
-    return (
-        <div className={`flex gap-8 md:gap-20 md:text-3xl mt-20 ${!countdown.init && 'invisible'}`}>
-            <div className="flex flex-col gap-6">
-                <h6 className={`py-2 md:text-[48px] lg:text-6xl font-semibold ${oswald.className}`}>{countdown.days}</h6>
-                <p className={`${robotoMono.className}`}>D</p>
-            </div>
-            <div className="flex flex-col gap-6">
-                <h6 className={`py-2 md:text-[48px] lg:text-6xl font-semibold ${oswald.className}`}>{countdown.hours}</h6>
-                <p className={`${robotoMono.className}`}>H</p>
-            </div>
-            <div className="flex flex-col gap-6">
-                <h6 className={`py-2 md:text-[48px] lg:text-6xl font-semibold ${oswald.className}`}>{countdown.minutes}</h6>
-                <p className={`${robotoMono.className}`}>M</p>
-            </div>
-            <div className="flex flex-col gap-6">
-                <h6 className={`py-2 md:text-[48px] lg:text-6xl font-semibold ${oswald.className}`}>{countdown.seconds}</h6>
-                <p className={`${robotoMono.className}`}>S</p>
-            </div>
-        </div>
-    );
+  useEffect(() => {
+    const timer = setTimeout(() => setShow(true), 50); 
+    return () => clearTimeout(timer);
+  }, []);
+
+  return (
+    <div
+      className={`
+        flex gap-8 md:gap-20 md:text-3xl mt-20 
+        transition-opacity duration-1000 
+        ${countdown.init && show ? "opacity-100" : "opacity-0"}
+        ${robotoMono.className}
+      `}
+    >
+      <div className="min-w-[3ch] text-center flex flex-col gap-5">
+        <h6 className={`py-2 md:text-[48px] lg:text-5xl font-semibold`}>{countdown.days}</h6>
+        <p className={`font-light`}>D</p>
+      </div>
+      <div className="flex flex-col gap-5">
+        <h6 className={`py-2 md:text-[48px] lg:text-5xl font-semibold`}>{countdown.hours}</h6>
+        <p className={`font-light`}>H</p>
+      </div>
+      <div className="flex flex-col gap-5">
+        <h6 className={`py-2 md:text-[48px] lg:text-5xl font-semibold`}>{countdown.minutes}</h6>
+        <p className={`font-light`}>M</p>
+      </div>
+      <div className="flex flex-col gap-5">
+        <h6 className={`py-2 md:text-[48px] lg:text-5xl font-semibold`}>{countdown.seconds}</h6>
+        <p className={`font-light`}>S</p>
+      </div>
+    </div>
+  );
 }
