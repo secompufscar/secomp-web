2ab3e8df16d4552ae76f77a177e6134af9ee4584
Enzo Murayama <enzomurayama@gmail.com>
2025-09-19 15:41:59 -0300

    refactor: faq

--- src/data/faq.js
+++ src/data/faq.js
@@ -4,11 +4,11 @@ export const faq = [
     texto: 'Para se inscrever na SECOMP, cadastre-se em nosso aplicativo. Após criar sua conta, basta acessar a tela inicial e clicar no botão de inscrição para participar do evento.'
   },
   {
-    titulo: 'Como faço para me inscrever em competições?',
+    titulo: 'Como participar das competições?',
     texto: 'As inscrições para as competições devem ser feitas por meio dos formulários disponíveis em nosso Instagram. Fique atento em nossas redes sociais!'
   },
   {
-    titulo: 'Como faço para me inscrever em minicursos?',
+    titulo: 'Como participar dos minicursos?',
     texto: 'A inscrição nos minicursos deve ser feita diretamente pelo nosso aplicativo. Lembre-se de estar previamente registrado no evento e de trazer 1 kg de alimento não perecível no dia da atividade.'
   },
   {
@@ -16,7 +16,11 @@ export const faq = [
     texto: 'Para receber o seu certificado de participação na SECOMP, é necessário estar inscrito no evento e registrar pelo menos 75% de presença nas atividades. Após o evento, o certificado será enviado para o e-mail cadastrado. Certifique-se de preencher todos os dados corretamente no momento da inscrição.'
   },
   {
-    titulo: 'Estou interessado em patrocinar a SECOMP! O que devo fazer?',
+    titulo: 'Quero patrocinar a SECOMP. Como faço?',
     texto: 'Se você deseja se tornar um patrocinador da SECOMP, entre em contato conosco através do e-mail. Forneceremos todas as informações sobre as oportunidades de patrocínio, benefícios e como sua empresa pode apoiar o evento. Não perca a chance de fazer parte desta experiência!'
+  },
+  {
+    titulo: 'Consigo ganhar prêmios durante o evento?',
+    texto: 'Sim! Os participantes que ficarem no Top 3 do ranking geral da SECOMP receberão prêmios especiais como reconhecimento pelo seu desempenho e engajamento ao longo do evento. Para acompanhar sua posição, acesse nosso aplicativo.'
   },
 ];
